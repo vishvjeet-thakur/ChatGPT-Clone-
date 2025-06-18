21d72d7dc56e810f1884bccd361e9fbe7942eac0
vishvjeet-thakur <vishwjeetthakur995@gmail.com>
2025-06-19 01:38:37 +0530

    made the font similar to chatgpt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
 
-      <body className={inter.className}>
+      <body >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
